aae040d028346fc98f4dbf29df84f82daf50f37d
Ha-Mim <hamim.hyder@gmail.com>
2015-05-13 13:00:46 +0600

    New File Added

---
+++ MethodApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodApp
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            double i = 11;
+
+            int a = 100;
+            long b = a;
+
+            long c = 20;
+            int d = (int)c;
+
+            double result = GetSquare(i);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+        private static double GetSquare(double aValue)
+        {
+         double square = aValue*aValue;
+            return square;
+        }
+
+
+    }
+}
